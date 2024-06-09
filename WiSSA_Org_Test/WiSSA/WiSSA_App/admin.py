90bab9da9adb7423f1bc369f6ee0d2e7aaa68bec
Stefan881 <ondeyostephen0@gmail.com>
2024-06-09 15:49:03 +0300

    test

---
+++ WiSSA_Org_Test/WiSSA/WiSSA_App/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import SubscribedUsers
+
+
+class SubscribedUsersAdmin(admin.ModelAdmin):
+    list_display = ('email','created_date')
+
+# Register your models here.
+
+
+admin.site.register(SubscribedUsers,SubscribedUsersAdmin)
